f5cec0c664367b80a4be62182da25687ae151e35
ace <acegit94@gmail.com>
2025-06-17 01:25:07 +0100

    Draw logic added

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,5 +1,10 @@
 """Game board"""
 BOARD_SIZE = 9
+WIN_PATTERNS = [
+    (0, 1, 2), (3, 4, 5), (6, 7, 8),  # rows
+    (0, 3, 6), (1, 4, 7), (2, 5, 8),  # cols
+    (0, 4, 8), (2, 4, 6)              # diagonals
+]
 
 def display_game_board(game_board: list):
     """Displays game board"""
@@ -11,11 +16,13 @@ def validate_position(position: str) -> bool:
     """Check if user input is a number between 1, 10"""
     return position.isdigit() and 1 <= int(position) <= BOARD_SIZE
 
-def update_position(position: str, player_symbol: str, game_board:list) -> bool:
+def update_position(position: str,
+                    player_symbol: str,
+                    game_board:list) -> bool:
     """Add X or O to player selected position
     return True if successfully added
     otherwise return False"""
-    if validate_position(position=position):
+    if validate_position(position):
         position = int(position)
         if game_board[position - 1] == " ":
             game_board[position-1] = player_symbol
@@ -23,94 +30,53 @@ def update_position(position: str, player_symbol: str, game_board:list) -> bool:
     print("Invalid position! Please try again")
     return False
 
-def x_or_o():
+def x_or_o_choice(user_input: str) -> tuple[str, str]:
     """Player selects between X or O"""
-    player_one_choice = input("Player one please select X or O \n").strip().upper()
-    if player_one_choice == "O":
+    if user_input == "O":
         return "O", "X"
     return "X", "O"
 
-def check_horizontal(game_board:list, player_one_symbol: str, player_two_symbol: str) -> bool:
-    """Check rows for winner"""
-    rows = [
-        (0, 1, 2),
-        (3, 4, 5),
-        (6, 7, 8)
-    ]
-    for a, b, c in rows:
-        if game_board[a] == game_board[b] == game_board[c]:
-            if game_board[a] == player_one_symbol:
-                print("Player one wins!")
-                return True
-            if game_board[a] == player_two_symbol:
-                print("Player two wins!")
-                return True
-    return False
+def check_winner(game_board: list,
+                 player_one_symbol: str,
+                 player_two_symbol: str) -> str:
+    """Return false is player has one, True otherwise to continue game"""
+    symbol_to_player = {player_one_symbol: "Player one", player_two_symbol: "Player two"}
 
-def check_vertical(game_board:list, player_one_symbol: str, player_two_symbol: str) -> bool:
-    """Check columns for winner"""
-    columns = [
-        (0,3,6),
-        (1,4,7),
-        (2,5,8)
-    ]
-    for a,b,c in columns:
-        if game_board[a] == game_board[b] == game_board[c]:
-            if game_board[a] == player_one_symbol:
-                print("Player one wins!")
-                return True
-            if game_board[a] == player_two_symbol:
-                print("Player two wins!")
-                return True
-    return False
+    for a, b, c in WIN_PATTERNS:
+        if game_board[a] == game_board[b] == game_board[c] != " ":
+            return  symbol_to_player[game_board[a]]
+    return None
 
-def check_diagonal(game_board:list, player_one_symbol: str, player_two_symbol: str) -> bool:
-    """Check diagonals for winner"""
-    diagonals = [
-        (0, 4, 8),
-        (2, 4, 6)
-    ]
-    for a, b, c in diagonals:
-        if game_board[a] == game_board[b] == game_board[c]:
-            if game_board[a] == player_one_symbol:
-                print("Player one wins")
-                return True
-            if game_board[a] == player_two_symbol:
-                print("Player two wins")
-                return True
-        return False
-
-def check_winner(player_one_symbol,
-                 player_two_symbol,
-                 game_board):
-    """Check if there is a winner"""
-    if check_horizontal(game_board=game_board, player_one_symbol=player_one_symbol,
-                        player_two_symbol=player_two_symbol) or check_vertical(
-                        game_board=game_board, player_one_symbol=player_one_symbol,
-                        player_two_symbol=player_two_symbol) or check_diagonal(
-                        game_board=game_board, player_one_symbol=player_one_symbol,
-                        player_two_symbol=player_two_symbol):
-        return False
-    return True
+def board_full(game_board: list) -> bool:
+    return " " in game_board
 
 def game_logic():
     """Game logic"""
-    playing = True
-    player_one_turn = True
     game_board = [" "] * BOARD_SIZE
-    player_one_symbol, player_two_symbol = x_or_o()
-    display_game_board(game_board=game_board)
-    while playing:
+    game_running = True
+    player_one_turn = True
+
+    player_one_symbol, player_two_symbol = x_or_o_choice(input("Player1 select X or O \n"))
+    display_game_board(game_board)
+
+    while game_running:
         if player_one_turn:
             player_choice = input("Player1 select a position between 1 to 9 \n")
-            if update_position(player_choice, player_one_symbol, game_board=game_board):
+            if update_position(player_choice, player_one_symbol, game_board):
                 player_one_turn = False
         else:
             player_choice = input("Player2 select a position between 1 to 9 \n")
-            if update_position(player_choice, player_two_symbol, game_board=game_board):
+            if update_position(player_choice, player_two_symbol, game_board):
                 player_one_turn = True
-        playing = check_winner(game_board=game_board, player_one_symbol=player_one_symbol,
-                                   player_two_symbol=player_two_symbol,
-                                   )
-        display_game_board(game_board=game_board)
+
+        winner = check_winner(game_board, player_one_symbol, player_two_symbol)
+        if winner:
+            print(f"{winner} wins!")
+            game_running = False
+
+        if not board_full(game_board):
+            print("Draw")
+            game_running = False
+
+        display_game_board(game_board)
 game_logic()
